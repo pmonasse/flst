65ebcd19d9cd782521ce8506b668db135d133772
Pascal Monasse <monasse@imagine.enpc.fr>
2018-06-14 10:44:18 +0200

    Compute TV in test.cpp

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include "libImage/image_io.hpp"
 #include "tree.h"
+#include <cstdlib>
 #include <iostream>
 
 int main(int argc, char* argv[]) {
@@ -15,7 +16,16 @@ int main(int argc, char* argv[]) {
 
     LsTree tree(im.data(), im.Width(), im.Height());
     std::cout << "Shapes: " << tree.iNbShapes << " "
-              << "Mem: " << (tree.iNbShapes*sizeof(LsShape)+tree.nrow*tree.ncol*sizeof(LsShape*))/1024/1024 <<  "MB" << std::endl;
+              << "Mem: " << (tree.iNbShapes*sizeof(LsShape)+tree.nrow*tree.ncol*sizeof(LsShape*))/1024/1024 <<  "MB ";
+
+    long int TV=0;
+    for(int i=0; i<im.Height(); i++)
+        for(int j=0; j+1<im.Width(); j++)
+            TV += abs(im(i,j)-im(i,j+1));
+    for(int i=0; i+1<im.Height(); i++)
+        for(int j=0; j<im.Width(); j++)
+            TV += abs(im(i,j)-im(i+1,j));
+    std::cout << "TV: " << TV << std::endl;
 
     return 0;
 }
