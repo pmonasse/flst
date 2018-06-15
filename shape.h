069c7f91a59f0e75960d6e438c9d49d7e4a38a4a
Pascal Monasse <monasse@imagine.enpc.fr>
2018-06-15 10:36:49 +0200

    Headers of files

--- shape.h
+++ shape.h
@@ -1,3 +1,21 @@
+/**
+ * SPDX-License-Identifier: MPL-2.0
+ * @file shape.h
+ * @brief Shape structure, for tree insertion
+ * @author Pascal Monasse <monasse@imagine.enpc.fr>
+ *
+ * Copyright (c) 2018 Pascal Monasse
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Pulic License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef SHAPE_H
 #define SHAPE_H
 #include <vector>
