2cad04616769881d01d79ba0d14e46dcf8779a4a
Pascal Monasse <pascal.monasse@enpc.fr>
2024-04-30 16:12:48 +0200

    Typo in comments

--- src/shape.h
+++ src/shape.h
@@ -5,20 +5,6 @@
  * @author Pascal Monasse <monasse@imagine.enpc.fr>
  *
  * Copyright (c) 2018 Pascal Monasse
- * All rights reserved.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Mozilla Public License as
- * published by the Mozilla Foundation, either version 2.0 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Mozilla Public License for more details.
- *
- * You should have received a copy of the Mozilla Public License
- * along with this program. If not, see <https://www.mozilla.org/en-US/MPL/2.0/>
  */
 
 #ifndef SHAPE_H
@@ -39,7 +25,7 @@ struct LsShape {
     Type type; ///< Inf or sup level set
     unsigned char gray; ///< Gray level of the level set
     bool bIgnore; ///< Should the shape be ignored?
-    bool bBoundary; ///< Does the shape meets the border of the image?
+    bool bBoundary; ///< Does the shape meet the border of the image?
 
     int area; ///< Number of pixels in the shape
     LsPoint* pixels; ///< Array of pixels in shape
